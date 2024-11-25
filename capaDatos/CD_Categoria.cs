974351232e650f1387ce91a3308680f7935f1b11
JARETZA CONCEPCION DE LEON BALCAZAR <jaretza.deb@uanl.edu.mx>
2024-11-24 20:32:59 -0600

    jarepestañas

---
+++ capaDatos/CD_Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaDatos
+{
+    public class CD_Categoria
+    {
+        public List<CD_Categoria> Listar()
+        {
+            List<CD_Categoria> lista = new List<CD_Categoria>();
+            try
+            {
+                using (SqlConnetion oconexion 
+            }
+        }
+    }
+}
